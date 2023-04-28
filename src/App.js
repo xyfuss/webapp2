c50db14cedb91fe8369a83f1749a28fa2c109d1a
xyfuss <jonasaatomren@gmail.com>
2023-04-28 11:41:42 +0200

    functional todo add/remove

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
+import React from 'react';
 import TodoList from "./TodoList";
-import React, { useState, useRef} from "react";
 
 
 function App() {
-  const [todos, setTodos] = useState([{id: 1, name: 'Todo 1'}])
-  const todoNameRef = useRef()
-  function handleAddTodo(e) {
-    const name = todoNameRef.current.value
-    if (name === "") return
-    setTodos(todos => [...todos, {name}])
-    todoNameRef.current.value = null
-  }
 
   
   return (
     <>
-    <TodoList todos={todos}/>
-    <input ref={todoNameRef}type="text"></input>
-    <button onClick={handleAddTodo}>Add todo</button>
-    <div>0 left to do</div>
+    <TodoList/>
     </>
     
   )
     
-}
+};
 
 export default App;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,11 +1,38 @@
-import React from 'react'
-import Todo from './Todo'
+import React from 'react';
+import { useState } from 'react';
 
-export default function TodoList({todos}) {
+export default function TodoList() {
+    const [todos, setTodos] = useState([
+        {id:1,text:"Todo 1",completed: false},
+        {id:2,text:"Todo 2",completed: false}
+      ]);
+      const [newTodoText, setNewTodoText] = useState('');
+      const handleCheckboxChange = (todo) => {
+        const updatedTodos = todos.filter(t => t.id !== todo.id);
+        setTodos(updatedTodos);
+      }
+      const handleNewTodoInputChange = (event) => {
+        setNewTodoText(event.target.value);
+      };
+      const handleAddTodoClick = () => {
+        const newTodo = {id: Date.now(), text: newTodoText, completed: false};
+        setTodos([...todos,newTodo]);
+        setNewTodoText('');
+      }
   return (
-    todos.map(todo => {
-        return <Todo key={todo.name} todo={todo} />
-    })
-    
+    <div>
+      <ul>
+        {todos.map(todo =>(
+          <li key={todo.id}>
+              <input type="checkbox" checked={todo.completed} onChange={() => handleCheckboxChange(todo)} />
+              <span>{todo.text}</span>
+          </li>
+        ))}
+      </ul>
+      <div>
+      <input type="text" value={newTodoText} onChange={handleNewTodoInputChange}/>
+      <button onClick={handleAddTodoClick}>Add Todo</button>
+      </div>
+    </div>
   )
 }
